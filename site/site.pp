f7225a244ed730252497b44633e6375c8206af26
fasseg <frank.asseg@googlemail.com>
2013-10-28 15:12:55 +0100

    updated WAR file location

--- site/site.pp
+++ site/site.pp
@@ -6,7 +6,7 @@ node default {
   }
 
   tomcat7::fcrepo-webapp { "fcrepo webapp":
-    url => 'http://ci.fcrepo.org/jenkins/job/fcrepo4/lastSuccessfulBuild/org.fcrepo\$fcrepo-webapp/artifact/org.fcrepo/fcrepo-webapp/4.0-SNAPSHOT/fcrepo-webapp-4.0-SNAPSHOT.war',
+    url => 'http://ci.fcrepo.org/jenkins/job/fcrepo4/lastSuccessfulBuild/org.fcrepo$fcrepo-webapp/artifact/org.fcrepo/fcrepo-webapp/4.0.0-alpha-3-SNAPSHOT/fcrepo-webapp-4.0.0-alpha-3-SNAPSHOT.war',
     war => 'fcrepo.war',
   }
 
